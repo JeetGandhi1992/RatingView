194b78e59dbc5035ca8e8ee409bf0a670587a4fa
Jeet Gandhi <jeet.gandhi@zuhlke.com>
2021-05-06 13:51:59 +0800

    Initial Commit

---
+++ RatingView/RatingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingViewApp.swift
+//  RatingView
+//
+//  Created by Jeet Gandhi on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct RatingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
